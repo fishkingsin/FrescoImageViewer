6411ee1b171b0087719b45c5d116a1c3995c911a
James Kong <james.kong@nixplay.com>
2017-07-25 18:01:52 +0800

    removed comment

--- frescoimageviewer/src/main/java/com/stfalcon/frescoimageviewer/drawee/ArcUtils.java
+++ frescoimageviewer/src/main/java/com/stfalcon/frescoimageviewer/drawee/ArcUtils.java
@@ -22,7 +22,7 @@ import static java.lang.Math.toRadians;
  * | . ` | | |  /   \ |  __/| |    |  _  |\ /
  * | |\  |_| |_/ /^\ \| |   | |____| | | || |
  * \_| \_/\___/\/   \/\_|   \_____/\_| |_/\_/
- * <p>
+ *
  * Created by jameskong on 25/7/2017.
  */
 
@@ -80,13 +80,7 @@ public final class ArcUtils {
         }
     }
 
-    /**
-     * Normalize the input radians in the range 360° > x >= 0°.
-     *
-     * @param radians The angle to normalize (in radians).
-     *
-     * @return The angle normalized in the range 360° > x >= 0°.
-     */
+
     public static double normalizeRadians(double radians)
     {
         radians %= FULL_CIRCLE_RADIANS;
@@ -131,34 +125,7 @@ public final class ArcUtils {
         return pointFromAngleRadians(center, radius, toRadians(angleDegrees));
     }
 
-    /**
-     * Adds a circular arc to the given path by approximating it through a cubic Bézier curve.
-     * <p/>
-     * <p>
-     * Note that this <strong>does not</strong> split the arc to better approximate it, for that see either:
-     * <ul>
-     * <li>{@link #createBezierArcDegrees(PointF, float, float, float, int, boolean,
-     * Path)}</li>
-     * <li>{@link #createBezierArcRadians(PointF, float, double, double, int, boolean,
-     * Path)}</li>
-     * </ul>
-     * </p>
-     * <p/>
-     * For a technical explanation:
-     * <a href="http://hansmuller-flex.blogspot.de/2011/10/more-about-approximating-circular-arcs.html">
-     * http://hansmuller-flex.blogspot.de/2011/10/more-about-approximating-circular-arcs.html
-     * </a>
-     *
-     * @param path        The path to add the arc to.
-     * @param center      The center of the circle.
-     * @param start       The starting point of the arc on the circle.
-     * @param end         The ending point of the arc on the circle.
-     * @param moveToStart If {@code true}, move to the starting point of the arc
-     *                    (see: {@link Path#moveTo(float, float)}).
-     *
-     * @see #createBezierArcDegrees(PointF, float, float, float, int, boolean, Path)
-     * @see #createBezierArcRadians(PointF, float, double, double, int, boolean, Path)
-     */
+
     public static void addBezierArcToPath(@NonNull Path path, @NonNull PointF center,
                                           @NonNull PointF start, @NonNull PointF end, boolean moveToStart)
     {
@@ -180,60 +147,7 @@ public final class ArcUtils {
         path.cubicTo(x2, y2, x3, y3, end.x, end.y);
     }
 
-    /**
-     * Adds a circular arc to the given path by approximating it through a cubic Bézier curve, splitting it if
-     * necessary. The precision of the approximation can be adjusted through {@code pointsOnCircle} and
-     * {@code overlapPoints} parameters.
-     * <p>
-     * <strong>Example:</strong> imagine an arc starting from 0° and sweeping 100° with a value of
-     * {@code pointsOnCircle} equal to 12 (threshold -> 360° / 12 = 30°):
-     * <ul>
-     * <li>if {@code overlapPoints} is {@code true}, it will be split as following:
-     * <ul>
-     * <li>from 0° to 30° (sweep 30°)</li>
-     * <li>from 30° to 60° (sweep 30°)</li>
-     * <li>from 60° to 90° (sweep 30°)</li>
-     * <li>from 90° to 100° (sweep 10°)</li>
-     * </ul>
-     * </li>
-     * <li>if {@code overlapPoints} is {@code false}, it will be split into 4 equal arcs:
-     * <ul>
-     * <li>from 0° to 25° (sweep 25°)</li>
-     * <li>from 25° to 50° (sweep 25°)</li>
-     * <li>from 50° to 75° (sweep 25°)</li>
-     * <li>from 75° to 100° (sweep 25°)</li>
-     * </ul>
-     * </li>
-     * </ul>
-     * </p>
-     * <p/>
-     * For a technical explanation:
-     * <a href="http://hansmuller-flex.blogspot.de/2011/10/more-about-approximating-circular-arcs.html">
-     * http://hansmuller-flex.blogspot.de/2011/10/more-about-approximating-circular-arcs.html
-     * </a>
-     *
-     * @param center            The center of the circle.
-     * @param radius            The radius of the circle.
-     * @param startAngleRadians The starting angle on the circle (in radians).
-     * @param sweepAngleRadians How long to make the total arc (in radians).
-     * @param pointsOnCircle    Defines a <i>threshold</i> (360° /{@code pointsOnCircle}) to split the Bézier arc to
-     *                          better approximate a circular arc, depending also on the value of {@code overlapPoints}.
-     *                          The suggested number to have a reasonable approximation of a circle is at least 4 (90°).
-     *                          Less than 1 will be ignored (the arc will not be split).
-     * @param overlapPoints     Given the <i>threshold</i> defined through {@code pointsOnCircle}:
-     *                          <ul>
-     *                          <li>if {@code true}, split the arc on every angle which is a multiple of the
-     *                          <i>threshold</i> (yields better results if drawing precision is required,
-     *                          especially when stacking multiple arcs, but can potentially use more points)</li>
-     *                          <li>if {@code false}, split the arc equally so that each part is shorter than
-     *                          the <i>threshold</i></li>
-     *                          </ul>
-     * @param addToPath         An existing path where to add the arc to, or {@code null} to create a new path.
-     *
-     * @return {@code addToPath} if it's not {@code null}, otherwise a new path.
-     *
-     * @see #createBezierArcDegrees(PointF, float, float, float, int, boolean, Path)
-     */
+
     @NonNull
     public static Path createBezierArcRadians(@NonNull PointF center, float radius, double startAngleRadians,
                                               double sweepAngleRadians, int pointsOnCircle, boolean overlapPoints,
@@ -289,60 +203,7 @@ public final class ArcUtils {
         return path;
     }
 
-    /**
-     * Adds a circular arc to the given path by approximating it through a cubic Bézier curve, splitting it if
-     * necessary. The precision of the approximation can be adjusted through {@code pointsOnCircle} and
-     * {@code overlapPoints} parameters.
-     * <p>
-     * <strong>Example:</strong> imagine an arc starting from 0° and sweeping 100° with a value of
-     * {@code pointsOnCircle} equal to 12 (threshold -> 360° / 12 = 30°):
-     * <ul>
-     * <li>if {@code overlapPoints} is {@code true}, it will be split as following:
-     * <ul>
-     * <li>from 0° to 30° (sweep 30°)</li>
-     * <li>from 30° to 60° (sweep 30°)</li>
-     * <li>from 60° to 90° (sweep 30°)</li>
-     * <li>from 90° to 100° (sweep 10°)</li>
-     * </ul>
-     * </li>
-     * <li>if {@code overlapPoints} is {@code false}, it will be split into 4 equal arcs:
-     * <ul>
-     * <li>from 0° to 25° (sweep 25°)</li>
-     * <li>from 25° to 50° (sweep 25°)</li>
-     * <li>from 50° to 75° (sweep 25°)</li>
-     * <li>from 75° to 100° (sweep 25°)</li>
-     * </ul>
-     * </li>
-     * </ul>
-     * </p>
-     * <p/>
-     * For a technical explanation:
-     * <a href="http://hansmuller-flex.blogspot.de/2011/10/more-about-approximating-circular-arcs.html">
-     * http://hansmuller-flex.blogspot.de/2011/10/more-about-approximating-circular-arcs.html
-     * </a>
-     *
-     * @param center            The center of the circle.
-     * @param radius            The radius of the circle.
-     * @param startAngleDegrees The starting angle on the circle (in degrees).
-     * @param sweepAngleDegrees How long to make the total arc (in degrees).
-     * @param pointsOnCircle    Defines a <i>threshold</i> (360° /{@code pointsOnCircle}) to split the Bézier arc to
-     *                          better approximate a circular arc, depending also on the value of {@code overlapPoints}.
-     *                          The suggested number to have a reasonable approximation of a circle is at least 4 (90°).
-     *                          Less than 1 will ignored (the arc will not be split).
-     * @param overlapPoints     Given the <i>threshold</i> defined through {@code pointsOnCircle}:
-     *                          <ul>
-     *                          <li>if {@code true}, split the arc on every angle which is a multiple of the
-     *                          <i>threshold</i> (yields better results if drawing precision is required,
-     *                          especially when stacking multiple arcs, but can potentially use more points)</li>
-     *                          <li>if {@code false}, split the arc equally so that each part is shorter than
-     *                          the <i>threshold</i></li>
-     *                          </ul>
-     * @param addToPath         An existing path where to add the arc to, or {@code null} to create a new path.
-     *
-     * @return {@code addToPath} if it's not {@code null}, otherwise a new path.
-     *
-     * @see #createBezierArcRadians(PointF, float, double, double, int, boolean, Path)
-     */
+
     @NonNull
     public static Path createBezierArcDegrees(@NonNull PointF center, float radius, float startAngleDegrees,
                                               float sweepAngleDegrees, int pointsOnCircle, boolean overlapPoints,
